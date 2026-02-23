c805a0d68a554699d1bcc12f1cc5fa838f7938d6
unknown <raphael.alcateia>
2026-02-23 16:03:55 -0300

    Add the first routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RegistroComponent } from './paginas/registro/registro.component';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [ RegistroComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
