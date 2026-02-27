5439845f3804dc54b7a10926a0f35a729fdb44b9
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2026-02-27 15:05:52 -0300

    new template with side bar and new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,19 @@ import { Routes } from '@angular/router';
 import { RegistroComponent } from './paginas/registro/registro.component';
 import { ListaPostagemComponent } from './paginas/lista-postagem/lista-postagem.component';
 import { AuthGuard } from './guards/auth.guard';
+import { LayoutComponent } from './componentes/layout/layout.component';
 export const routes: Routes = [
     {
         path: 'registro', component: RegistroComponent
     },
     {
-        path: 'posts',canActivate: [AuthGuard] ,component: ListaPostagemComponent
+        path: "", 
+        component: LayoutComponent,
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: "posts" , component: ListaPostagemComponent
+            }
+        ]
     }
 ];
